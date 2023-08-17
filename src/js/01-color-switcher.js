e600f65a79cca2477b05fba277f9cc5e57518653
OlegPanadiy <opanadiy@gmail.com>
2023-08-17 20:49:32 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const elements = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+elements.btnStart.addEventListener('click', handlerOnStart);
+elements.btnStop.addEventListener('click', handlerOnStop);
+
+let timerId = null;
+
+function handlerOnStart() {
+  timerId = setInterval(
+    () => (
+      (elements.btnStart.disabled = true),
+      (elements.body.style.backgroundColor = getRandomHexColor())
+    ),
+    1000
+  );
+}
+function handlerOnStop() {
+  elements.btnStart.disabled = false;
+  clearInterval(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
