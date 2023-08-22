546e6202585645d198a418a2a392fd166069ef5b
olehpanadii <92667024+olehpanadii@users.noreply.github.com>
2023-08-22 20:05:32 +0000

    Deploying to gh-pages from @ olehpanadii/goit-js-hw-09@33eaddfec5a13fda5b4f2e0d7b8b4862866e6836 🚀

---
+++ 01-color-switcher.0ca8f3b1.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(function(){e=setInterval((()=>(t.btnStart.disabled=!0,t.btnStop.disabled=!1,t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`)),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(e)}));let e=null;t.btnStop.disabled=!0;
+//# sourceMappingURL=01-color-switcher.0ca8f3b1.js.map
